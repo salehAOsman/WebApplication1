66d2213a4d6637d463399523d7ac22af8d24fc8f
Saleh Osman <saleh1.a.osman@gmail.com>
2018-01-04 09:10:35 +0100

    added CkeckingTmp Model

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Get CheckTemp
+        public ActionResult CheckTemp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CheckTemp(float tmpIn=0)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/CheckingTmp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class CheckingTmp
+    {
+        //when method is static must be properties static 
+        static public float Temp { get; set; }
+        static public string Describe { get; set; }
+        static public string Recomend { get; set; }
+       // static public string InputNum { get; set; }
+
+        public CheckingTmp(float temp)
+        {
+            Temp = temp;
+            CalcTempeture();
+        }
+
+        static void CalcTempeture()
+        {
+            //if (Temp == 0)
+            //{
+            //    InputNum = "please, input number";
+            //}
+            if (Temp <= 30)
+            {
+                Describe = "Temperature is low";
+                Recomend = "Drink Limon and cover your self ";
+            }
+            else if (Temp > 30 && Temp <= 38)
+            {
+                Describe = "Temperature is normal";
+                Recomend = "Have as nice day";
+            }
+            else
+            {
+                Describe = "temperature too height";
+                Recomend = "Go to hospital";
+            }
+        }
+    }
+}
